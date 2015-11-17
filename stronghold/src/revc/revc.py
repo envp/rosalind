bf08bdabac1f01810988f811119d8c22b2601bb2
Vaibhav Yenamandra <yvvaibhav@gmail.com>
2015-11-17 06:43:17 +0000

    Updated revc: Removed unecessary OOP stuff

--- stronghold/src/revc/revc.py
+++ stronghold/src/revc/revc.py
@@ -1,31 +1,34 @@
-class DNA(object):
-    """
-        Compute the reverse complement of a DNA string
+"""
+    Compute the reverse complement of a DNA string
 
-        Given: A DNA string s of length at most 1000 bp.
+    Given: A DNA string s of length at most 1000 bp.
 
-        Return: The reverse complement sc of s.
-    """
+    Return: The reverse complement sc of s.
+"""
 
-    NUCLEOTIDES = ['a', 'c', 'g', 't']
+NUCLEOTIDES = ['a', 'c', 'g', 't']
 
-    COMPLEMENT_MAP = {'a': 't', 'c': 'g', 'g': 'c', 't': 'a'}
+COMPLEMENT_MAP = {'a': 't', 'c': 'g', 'g': 'c', 't': 'a'}
 
-    def __init__(self, string):
-        self.string = string.lower()
-
-    def reverse_complement(self):
-        if self.COMPLEMENT_MAP:
-            rev = [self.COMPLEMENT_MAP[s] for s in reversed(self.string)]
-            return ''.join(rev)
-
-
+def reverse_complement(string):
+    is_lower = string.islower()
+    is_upper = string.isupper()
+    string = string.lower()
+    revc = ''.join([COMPLEMENT_MAP[s] for s in reversed(string)])
+    
+    if is_upper and not is_lower:
+        # Originally uppercase
+        return revc.upper()
+    else:
+        # Originally lowercase / mixed case
+        return revc.lower()
+        
+        
 if __name__ == "__main__":
     import sys
 
     if len(sys.argv) < 2:
         print "Usage: python %s <STRING>" % sys.argv[0]
     else:
-        string = sys.argv[1]
-        dna = DNA(string)
-        print dna.reverse_complement().upper()
+        dna = sys.argv[1]
+        print reverse_complement(dna)
