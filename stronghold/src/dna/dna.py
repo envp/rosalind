e08bc3cee14ebb6486effc981f48652b724b70bb
Vaibhav Yenamandra <yvvaibhav@gmail.com>
2015-11-17 06:29:33 +0000

    Removed unecessary OOP paradigm from dna

--- stronghold/src/dna/dna.py
+++ stronghold/src/dna/dna.py
@@ -1,22 +1,19 @@
-class DNA(object):
-    """
-        Count the number of different in a given DNA string
+"""
+    Count the number of different in a given DNA string
 
-        Given: A DNA string s of length at most 1000 nt.
+    Given: A DNA string s of length at most 1000 nt.
 
-        Return: Four integers (separated by spaces) counting 
-                the respective number of times that the symbols 
-                'A', 'C', 'G', and 'T' occur in s.
-    """
+    Return: Four integers (separated by spaces) counting 
+            the respective number of times that the symbols 
+            'A', 'C', 'G', and 'T' occur in s.
+"""
 
-    NUCLEOTIDES = ['a', 'c', 'g', 't']
+NUCLEOTIDES = ['a', 'c', 'g', 't']
 
-    def __init__(self, string):
-        self.string = string.lower()
-
-    def count_nucleotides(self):
-        base_count = {x: self.string.count(x) for x in self.NUCLEOTIDES}
-        return base_count
+def count_nucleotides(string):
+    string = string.lower()
+    base_count = {x: string.count(x) for x in NUCLEOTIDES}
+    return base_count
 
 
 if __name__ == "__main__":
@@ -25,8 +22,7 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print "Usage: python %s <STRING>" % sys.argv[0]
     else:
-        string = sys.argv[1]
-        dna = DNA(string)
-        base_count = dna.count_nucleotides()
-        strigified_count = [str(base_count[k]) for k in dna.NUCLEOTIDES]
-        print ' '.join(strigified_count)
+        dna = sys.argv[1]
+        base_count = count_nucleotides(dna)
+        stringified_count = [str(base_count[k]) for k in NUCLEOTIDES]
+        print ' '.join(stringified_count)
