0bff85bc313f8594b972663c3925343c99eef40a
Vaibhav Yenamandra <yvvaibhav@gmail.com>
2015-11-17 06:49:25 +0000

    Updated RNA to remove needless OOP

--- stronghold/src/rna/rna.py
+++ stronghold/src/rna/rna.py
@@ -1,22 +1,28 @@
-class DNA(object):
-    """
-        Transcribe a DNA string to a RNA string
+"""
+    Transcribe a DNA string to a RNA string
 
-        Given: A DNA string t having length at most 1000 nt.
+    Given: A DNA string t having length at most 1000 nt.
 
-        Return: The transcribed RNA string of t.
-    """
+    Return: The transcribed RNA string of t.
+"""
 
-    NUCLEOTIDES = ['a', 'c', 'g', 't']
+NUCLEOTIDES = ['a', 'c', 'g', 't']
 
-    COMPLEMENT_MAP = {'a': 't', 'c': 'g', 'g': 'c', 't': 'a'}
+COMPLEMENT_MAP = {'a': 't', 'c': 'g', 'g': 'c', 't': 'a'}
 
-    def __init__(self, string):
-        self.string = string.lower()
 
-    def to_rna(self):
-        return self.string.replace('t', 'u')
-
+def to_rna(string):
+    is_upper = string.isupper()
+    is_lower = string.islower()
+    string = string.lower()
+    string = string.replace('t', 'u')
+    
+    if is_upper and not is_lower:
+        # Originally uppercase
+        return string.upper()
+    else:
+        # Originally lowercase / mixed case
+        return string.lower()
 
 if __name__ == "__main__":
     import sys
@@ -24,7 +30,6 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print "Usage: python %s <STRING>" % sys.argv[0]
     else:
-        string = sys.argv[1]
-        dna = DNA(string)
-        transcribed = dna.to_rna()
+        dna = sys.argv[1]
+        transcribed = to_rna(dna)
         print transcribed.upper()
