ca015fd198e7317ddd47e27130f3251626630c42
Vaibhav Yenamandra <yvvaibhav@gmail.com>
2015-11-18 17:42:52 +0000

    Autopep8 IPRB

--- stronghold/src/iprb/iprb.py
+++ stronghold/src/iprb/iprb.py
@@ -1,18 +1,18 @@
 """
     http://rosalind.info/problems/iprb/
-    
-    Given the populations of various genotypes, determine the probability of a 
+
+    Given the populations of various genotypes, determine the probability of a
     specific phenotype emerging in the next generation
-    
-    Given:  Three positive integers k, m, and n, representing a population 
-            containing k+m+n organisms: 
-            k individuals are homozygous dominant for a factor, 
-            m are heterozygous, and 
+
+    Given:  Three positive integers k, m, and n, representing a population
+            containing k+m+n organisms:
+            k individuals are homozygous dominant for a factor,
+            m are heterozygous, and
             n are homozygous recessive.
 
-    Return: The probability that two randomly selected mating organisms will 
-            produce an individual possessing a dominant allele 
-            (and thus displaying the dominant phenotype). 
+    Return: The probability that two randomly selected mating organisms will
+            produce an individual possessing a dominant allele
+            (and thus displaying the dominant phenotype).
             Assume that any two organisms can mate.
 """
 from __future__ import division
@@ -26,6 +26,7 @@ def comb(n, r):
         return n
     else:
         return (n / r) * comb(n - 1, r - 1)
+
 
 def iprb(k, m, n):
     """
@@ -44,7 +45,7 @@ def iprb(k, m, n):
               |X |x |
               +--+--+
              x|Xx|xx|
-              +--+--+             
+              +--+--+
              x|Xx|xx|
               +--+--+
               |X |x |
@@ -53,15 +54,16 @@ def iprb(k, m, n):
               +--+--+
              x|Xx|xx|
     """
-    total_matings = 4 * (k*m + k*n + n*m +comb(k, 2) + comb(n, 2) + comb(m, 2))
-    dominant = 4 * (comb(k, 2) + k*m + k*n) + 3 * comb(m, 2) + 2*m*n
-    
+    total_matings = 4 * (k * m + k * n + n * m +
+                         comb(k, 2) + comb(n, 2) + comb(m, 2))
+    dominant = 4 * (comb(k, 2) + k * m + k * n) + 3 * comb(m, 2) + 2 * m * n
+
     return dominant / total_matings
-    
+
 
 if __name__ == "__main__":
     import sys
-    
+
     if len(sys.argv) < 4:
         print "Usage: python %s <k> <m> <n>" % sys.argv[0]
     else:
